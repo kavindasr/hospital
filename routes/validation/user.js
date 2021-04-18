cc344eb7723605336e7657ee7f4635f26c36cc26
kavinda <kavindasr@icloud.com>
2021-04-18 10:54:35 +0530

    role_id bug fixed

--- routes/validation/user.js
+++ routes/validation/user.js
@@ -9,7 +9,7 @@ const userRegSchema = Joi.object().keys({
     nic: Joi.string().length(12).pattern(/^[0-9]+$/).required(),
     user_name: Joi.string().max(40).required(),
     pswrd: Joi.string().min(6).required(),
-    role: Joi.number().required(),
+    role_id: Joi.number().required(),
 });
 
 module.exports = { loginSchema, userRegSchema }
