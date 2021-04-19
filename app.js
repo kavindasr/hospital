76053a8539f8b22466be7137fbc257e88c07f954
kavinda <kavindasr@icloud.com>
2021-04-19 12:46:04 +0530

    upload fromdata structure

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(auth);
+// app.use(auth);
+app.use('/uploads', express.static('uploads/'));
 indexRouter(app);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const nurseRouter = require('./api/nurse');
 const userRouter = require('./api/user');
 const commanRouter = require('./api/comman');
+const uploadRouter = require('./api/upload');
 
 const endPointHandler = (app) => {
   app.use('/user', userRouter);
   app.use('/nurse', nurseRouter);
   app.use('/comman', commanRouter);
-
+  app.use('/upload', uploadRouter);
 }
 
 module.exports = endPointHandler;
