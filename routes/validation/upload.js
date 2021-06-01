e2e8face6ddb73fa69081840c6bd6a760abaf82f
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-06-02 00:50:17 +0530

    Request file changes

--- routes/validation/upload.js
+++ routes/validation/upload.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const uploadSchema = Joi.object().keys({
     reqId:Joi.number().required(),
     test_status: Joi.string().allow('Completed', 'Rejected').required(),
-    feedback: Joi.string(),
+    feedback: Joi.string().allow(''),
 });
 
 module.exports = { uploadSchema }
