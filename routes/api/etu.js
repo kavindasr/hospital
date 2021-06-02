f8654b3718be9337ab1dba46d1e4e4dbd1b605c2
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2021-06-02 21:47:04 +0530

    cc unit added

--- routes/api/etu.js
+++ routes/api/etu.js
@@ -104,4 +104,14 @@ router.get('/ccUnit', async (req, res, next) => {
         next(err);
     }
 });
+
+router.get('/ccUnit', async (req, res, next) => {
+    try{
+        const data = await admittedPatients();
+        res.status(200).render('etu/ccUnit', {data});
+    }
+    catch(err) {
+        next(err);
+    }
+});
 module.exports = router;
